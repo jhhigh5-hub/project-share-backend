a61d0953b213dcd0448da7a4c4d59cfb6324e789
jh <jhigh5@naver.com>
2025-12-12 12:54:25 +0900

    프로젝트 초기화

---
+++ src/main/java/com/example/sharebackend/ShareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareBackendApplication.class, args);
+    }
+
+}
