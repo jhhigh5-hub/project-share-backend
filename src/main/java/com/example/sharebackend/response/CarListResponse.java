80f680c7b08ff7de011af281f7b4baace38671fa
byoung-uk213 <hhtt2503@gmail.com>
2025-12-17 09:25:41 +0900

    자동차 검색 API 완료

--- src/main/java/com/example/sharebackend/response/CarListResponse.java
+++ src/main/java/com/example/sharebackend/response/CarListResponse.java
@@ -1,6 +1,7 @@
 package com.example.sharebackend.response;
 
 import com.example.sharebackend.domain.Car;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +12,9 @@ import java.util.List;
 @Setter
 @Getter
 @Builder
+@JsonPropertyOrder({"success", "total", "carList"})
 public class CarListResponse {
     boolean success;
     List<Car> carList;
-    int countAllCar;
+    int total;
 }
